7107de4268400d645558531ad40ea1dc10d6031c
sgarjala <sg@nvista.biz>
2021-02-15 20:53:55 +0100

    create Todo model and database

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import  SQLAlchemy
+from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+#from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False, index=True, unique=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) 
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 @app.route('/')
 def index():
     return render_template('index.html')
